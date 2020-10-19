c9ad28fbf58dbcc36c81b5d631a05c611557e7bc
mehulbhatt45 <mehul.2287884@gmail.com>
2020-10-19 18:18:31 +0530

    Base url changed to bluediamondvoyage

--- src/app/config .ts
+++ src/app/config .ts
@@ -2,7 +2,7 @@
 // const baseUrl = 'http://192.168.1.118/wordpress-trip_demo/wp-json/api/v2/';
 // const baseUrl = 'http://122.170.44.56/tripion/wp-json/api/v2/';
 
-const baseUrl = 'https://tripion-demo.mylionsgroup.com/wp-json/api/v2/';
+const baseUrl = 'https://bluediamondvoyage.com/wp-json/api/v2/';
 //
 // const baseUrl = 'http://localhost/wordpress-trip_demo/wp-json/api/v2/';
 // const mediaUrl = "http://192.168.1.118/wordpress-trip_demo/wp-content/uploads/";
